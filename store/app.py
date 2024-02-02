39612b8521e33e78bda5a6f8987d121f02df67a9
Anusha Ranganathan <anusha@cottagelabs.com>
2024-02-03 01:24:15 +0530

    Extended store to manage file backups

--- store/app.py
+++ store/app.py
@@ -4,15 +4,16 @@ from flask import Flask, request, send_file, abort, make_response, logging
 import logging
 from logging.handlers import RotatingFileHandler
 from store.lib.storage_tree import StorageTree
+from store.lib.backup_helper import BackupHelper
 
 app = Flask(__name__)
 app.config['HOST'] = "localhost"
 app.config['DEBUG'] = False
 app.config['PORT'] = 5999
 app.config['THREADED'] = True
-app.config['STORAGE_FOLDER'] = '/home/green/jperstore'
+app.config['STORAGE_FOLDER'] = '/home/anusha/Documents/src/deepgreen/jperstore'
 
-file_handler = RotatingFileHandler('/home/green/jperstorelog', maxBytes=10000000, backupCount=5)
+file_handler = RotatingFileHandler('/home/anusha/Documents/src/deepgreen/jperstorelog', maxBytes=10000000, backupCount=5)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(logging.Formatter(
     '%(asctime)s %(levelname)s: %(message)s '
@@ -76,7 +77,34 @@ def storage(path=''):
     else:
         abort(400)
 
-
+@app.route('/backup/<path:path>', methods=['GET','PUT','DELETE'])
+def storage_backup(path=''):
+    # the backup file ends with .bak_n
+    if '..' in path or path.startswith('/'):
+        abort(400)
+    dir = st.tree_path(path)
+    if path == '':
+        abort(400)
+    if not os.path.exists(dir):
+        abort(404)
+    if os.path.isdir(dir):
+        abort(400)
+    if request.method == 'DELETE' or ( request.method == 'POST' and request.form.get('submit','').lower() == 'delete' ):
+        bak_helper = BackupFilesHelper(dir)
+        bak_helper.delete_backup_files()
+        return ''
+    elif request.method == 'PUT':
+        bak_helper = BackupFilesHelper(dir)
+        bak_helper.make_new_backup_file()
+        return ''
+    elif request.method == 'GET':
+        bak_helper = BackupFilesHelper(dir)
+        listing = bak_helper.get_backup_files()
+        resp = make_response( json.dumps( listing ) )
+        resp.mimetype = "application/json"
+        return resp
+    else:
+        abort(400)
 if __name__ == "__main__":
     if not os.path.exists(app.config['STORAGE_FOLDER']):
         print('Storage folder does not exist!')
---
+++ store/lib/backup_helper.py
@@ -0,0 +1,62 @@
+import os, glob
+
+class BackupHelper:
+    def __init__(self, file_path):
+        if not os.path.exists(file_path):
+            raise Exception("path does not exist")
+        if not os.path.isfile(file_path):
+            raise Exception("Path should be a file")
+        self.file_path = file_path
+        self.path = os.path.dirname(file_path)
+        self.base_file_name = os.path.basename(file_path)
+        self.all_backup_files = []
+        self.new_backup_file = None
+        self.current_version = None
+        self.new_version = None
+
+    def valid_backup_file_name(self):
+        if self.backup_file_name is None:
+            return false
+        is_backup_file = False
+        matches_base_file = False
+        base_file_name = self.backup_file_name.rsplit('.bak', 1)[0]
+        pattern = re.compile(r"\.bak_[0-9][0-9]$")
+        if pattern.match(self.backup_file_name):
+            is_backup_file = True
+        if self.base_file_name == base_file_name:
+            matches_base_file = True
+        return is_backup_file and matches_base_file
+
+    def get_backup_files(self):
+        return glob.glob(f"{self.path}/*.bak*")
+
+    def get_last_backup_file(self):
+        return max(get_backup_files(), key=os.path.getctime)
+
+    def get_last_version(self):
+        last_backup_file = self.get_last_backup_file()
+        last_version = last_backup_file.rsplit('.bak', 1)[1]
+        if last_version:
+            return last_version.to_i
+        else:
+            return 0
+
+    def get_new_backup_file(self):
+        new_version = self.get_last_version() + 1
+        new_backup_file_name = f"{self.base_file_name}.bak_{new_version}"
+        self.new_backup_file = os.path.join(self.path, new_backup_file_name)
+        return self.new_backup_file
+
+    def make_new_backup_file(self):
+        get_new_backup_file()
+        if not os.path.exists(self.new_backup_file):
+            shutil.copy(self.file_path, self.new_backup_file())
+            return True
+        return False
+
+    def delete_backup_files(self):
+        backup_files = self.get_backup_files()
+        for backup_file in backup_files:
+            if os.path.exists(backup_file) and os.path.isfile(backup_file):
+                os.remove(backup_file)
+        return
